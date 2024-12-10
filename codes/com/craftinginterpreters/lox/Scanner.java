965d335ed9dd3ed9629e10d65fd91a0ddceb6654
cito22040 <cito22040@g.nihon-u.ac.jp>
2024-12-10 12:00:52 +0900

    modified Scanner.java for identifier

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -14,6 +14,28 @@ class Scanner {
   private int current = 0;
   private int line = 1;
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
@@ -27,6 +49,17 @@ class Scanner {
     if (current + 1 >= source.length()) return '\0';
     return source.charAt(current + 1);
   } 
+
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
@@ -75,11 +108,22 @@ class Scanner {
       
     case '"': string(); break;
     
+    case 'o':
+    if (match('r')) {
+      addToken(OR);
+    }
+    break;
+
+
      default:
       if (isDigit(c)) 
       {
         number();
       } 
+      else if (isAlpha(c))
+      {
+        identifier();
+      }
       else
       {
         Lox.error(line, "Unexpected character.");
@@ -87,6 +131,16 @@ class Scanner {
         break;
     }
   }
+
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(IDENTIFIER);
+  }
+
 
 
   private void number() {
