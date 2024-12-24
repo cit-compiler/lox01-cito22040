a7255878284a3725101015afec6e6fa7bf981ee0
cito22040 <cito22040@g.nihon-u.ac.jp>
2024-12-24 11:05:34 +0900

    Expr.java

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -1,11 +1,48 @@
-static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+abstract class Expr {
+    interface Visitor<R> {
+      R visitBinaryExpr(Binary expr);
     }
-
-   final Expr left;
-    final Token operator;
-    final Expr right;
+  
+    abstract <R> R accept(Visitor<R> visitor);
+  
+    static class Binary extends Expr {
+      final Expr left;
+      final Token operator;
+      final Expr right;
+  
+      Binary(Expr left, Token operator, Expr right) {
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
+      }
+  
+      @Override
+      <R> R accept(Visitor<R> visitor) {
+        return visitor.visitBinaryExpr(this);
+      }
+    }
   }
+  
+  interface PastryVisitor {
+      void visitBeignet(Beignet beignet); 
+      void visitCruller(Cruller cruller);
+  }
+  
+  abstract class Pastry {
+      abstract void accept(PastryVisitor visitor);
+  }
+  
+  class Beignet extends Pastry {
+      @Override
+      void accept(PastryVisitor visitor) {
+          visitor.visitBeignet(this);
+      }
+  }
+  
+  class Cruller extends Pastry {
+      @Override
+      void accept(PastryVisitor visitor) {
+          visitor.visitCruller(this);
+      }
+  }
+  
