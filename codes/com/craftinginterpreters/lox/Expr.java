5b28f9af38d0bed204ae99ec534db119541172d0
cito22040 <cito22040@g.nihon-u.ac.jp>
2024-12-24 10:34:55 +0900

    GenerateAst.javac

---
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,11 @@
+static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+   final Expr left;
+    final Token operator;
+    final Expr right;
+  }
